ec478c715e941c7585b54a030f2b82562d229468
Daniel Mathiot <d.danymat@gmail.com>
2021-06-04 16:37:36 +0200

    Add completion for links
    This commit will add completion when the user types [[.
    Be careful, for sorting the links, the client needs to sort the values
    (at the moment).
    Please checkout https://github.com/hrsh7th/nvim-compe/issues/375 for
    related issue for nvim-compe

--- server/out/server.js
+++ server/out/server.js
@@ -2,15 +2,19 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var node_1 = require("vscode-languageserver/node");
 var vscode_languageserver_textdocument_1 = require("vscode-languageserver-textdocument");
+var fs_1 = require("fs");
 var documents = new node_1.TextDocuments(vscode_languageserver_textdocument_1.TextDocument);
 var connection = node_1.createConnection(node_1.ProposedFeatures.all);
-connection.onInitialize(function () {
+var workspaceFolders = [];
+connection.onInitialize(function (params) {
+    params.workspaceFolders;
+    workspaceFolders = params.workspaceFolders;
     var result = {
         capabilities: {
             textDocumentSync: node_1.TextDocumentSyncKind.Incremental,
             completionProvider: {
                 resolveProvider: true,
-                triggerCharacters: ["#"]
+                triggerCharacters: ["#", "["]
             }
             //hoverProvider: true
         }
@@ -22,14 +26,67 @@ connection.onInitialized(function () {
 });
 connection.onCompletion(function (params) {
     var _a;
-    var test = (_a = params.partialResultToken) === null || _a === void 0 ? void 0 : _a.toString();
-    var messages = [];
-    var item = { label: "#foo" };
-    messages.push(item);
-    item = { label: "#bar" + test };
-    messages.push(item);
+    var line = params.position.line;
+    var char = params.position.character;
+    var messages = { isIncomplete: false, items: [] };
+    var document = documents.get(params.textDocument.uri);
+    if (((_a = params.context) === null || _a === void 0 ? void 0 : _a.triggerCharacter) == "#") {
+        // Creating messages
+        // // TODO
+        var item = { label: "#foo" };
+        messages.items.push(item);
+        // Resets if the user press space button
+        if (document) {
+            var text = document.getText({
+                start: { line: line, character: char - 1 },
+                end: { line: line, character: char },
+            });
+            if (text == " ") {
+                return null;
+            }
+            if (text == "s") {
+                item = { label: "#salut" };
+                messages.items.push(item);
+            }
+        }
+    }
+    else {
+        if (document) {
+            var text = document.getText({
+                start: { line: line, character: char - 2 },
+                end: { line: line, character: char }
+            });
+            var beforeText = document.getText({
+                start: { line: line, character: char - 3 },
+                end: { line: line, character: char - 2 }
+            });
+            if (beforeText == '[') {
+                return null;
+            }
+            else if (text == '[[') {
+                var item_1 = { label: "" };
+                getFileNames().forEach(function (file) {
+                    item_1 = { label: file };
+                    messages.items.push(item_1);
+                });
+            }
+            else
+                return null;
+        }
+    }
     return messages;
 });
+function getFileNames() {
+    var uri = workspaceFolders.map(function (ws) { return ws.name; });
+    var files = [];
+    uri.forEach(function (u) {
+        fs_1.readdirSync(u).forEach(function (file) {
+            if (!file.startsWith('.'))
+                files.push(file.slice(0, -3));
+        });
+    });
+    return files;
+}
 connection.onCompletionResolve(function (item) {
     item.detail = 'This is a special hello world function';
     item.documentation = {
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,58 +1,112 @@
 import { TextDocuments, createConnection, InitializeResult, ProposedFeatures, 
-TextDocumentSyncKind, Hover, CompletionList, CompletionParams, CompletionItem, 
-MarkupKind } from "vscode-languageserver/node";
+    TextDocumentSyncKind, Hover, CompletionList, CompletionParams, CompletionItem, 
+    MarkupKind, 
+    WorkspaceFolder} from "vscode-languageserver/node";
 
-import { TextDocument } from "vscode-languageserver-textdocument";
+    import { TextDocument } from "vscode-languageserver-textdocument";
+    import { readdirSync } from "fs"
 
-const documents = new TextDocuments(TextDocument);
+    const documents = new TextDocuments(TextDocument);
+    let connection = createConnection(ProposedFeatures.all);
+    let workspaceFolders: WorkspaceFolder[] = []
 
-let connection = createConnection(ProposedFeatures.all);
-
-connection.onInitialize(() => {
-    const result: InitializeResult = {
-        capabilities: {
-            textDocumentSync : TextDocumentSyncKind.Incremental,
-            completionProvider: {
-                resolveProvider: true,
-                triggerCharacters: ["#"]
+    connection.onInitialize((params) => {
+        params.workspaceFolders
+        workspaceFolders = params.workspaceFolders!
+        const result: InitializeResult = {
+            capabilities: {
+                textDocumentSync : TextDocumentSyncKind.Incremental,
+                completionProvider: {
+                    resolveProvider: true,
+                    triggerCharacters: ["#", "["]
+                }
+                //hoverProvider: true
             }
-            //hoverProvider: true
         }
+        return result;
+    })
+
+    connection.onInitialized(() => {
+        connection.console.log("Initialized");
+    })
+
+    connection.onCompletion((params: CompletionParams) => {
+        let line = params.position.line
+        let char = params.position.character
+        let messages: CompletionList = { isIncomplete: false, items : [] }
+
+        let document = documents.get(params.textDocument.uri)
+        if (params.context?.triggerCharacter == "#") {
+
+            // Creating messages
+            // // TODO
+            let item: CompletionItem = { label: "#foo" }
+            messages.items.push(item)
+
+            // Resets if the user press space button
+            if ( document ) {
+                let text = document.getText({
+                    start: {line: line , character: char - 1},
+                    end: {line: line, character: char},
+                })
+                if (text == " ") { return null }
+                if (text == "s") { item = { label: "#salut" }; messages.items.push(item) }
+            }
+        }
+        else {
+            if (document) {
+                let text = document.getText({ 
+                    start: {line: line, character: char - 2 },
+                    end: {line: line, character: char}
+                })
+                let beforeText = document.getText({
+                    start: { line: line, character: char - 3 },
+                    end: { line: line, character: char - 2 }
+                })
+                if ( beforeText == '[' ) { return null }
+                else if ( text == '[[' ) {
+                    let item: CompletionItem = { label: "" }
+                    getFileNames().forEach((file) => { 
+                        item = {label: file } 
+                        messages.items.push(item)
+                    })
+                }
+                else return null
+        }
+        }
+        return messages
+    })
+
+    function getFileNames() {
+        let uri = workspaceFolders.map((ws) => ws.name)
+        let files: string[] = []
+        uri.forEach(u => {
+            readdirSync(u).forEach(file => { 
+                if ( !file.startsWith('.') )
+                    files.push(file.slice(0, -3)) 
+            })
+        })
+        return files
     }
-    return result;
-})
 
-connection.onInitialized(() => {
-    connection.console.log("Initialized");
-})
+    connection.onCompletionResolve((item): CompletionItem => {
 
-connection.onCompletion((params: CompletionParams) => {
-    let test = params.partialResultToken?.toString()
-    let messages: CompletionItem[] = []
-    let item: CompletionItem = { label: "#foo" }
-    messages.push(item)
-    item = { label: "#bar"+ test}
-    messages.push(item)
-    return messages
-})
-
-connection.onCompletionResolve((item): CompletionItem => {
-	item.detail = 'This is a special hello world function';
-	item.documentation =  {
-		kind: MarkupKind.Markdown,
-		value: [
-			'# Heading',
-			'```typescript',
-			'console.log("Hello World");',
-			'```'
-		].join('\n')};
-	return item;
-});
+        item.detail = 'This is a special hello world function';
+        item.documentation =  {
+            kind: MarkupKind.Markdown,
+            value: [
+                '# Heading',
+                '```typescript',
+                'console.log("Hello World");',
+                '```'
+            ].join('\n')};
+            return item;
+    });
 
 
-documents.onWillSave((event) => {
-	connection.console.log('On Will save received');
-});
+    documents.onWillSave((event) => {
+        connection.console.log('On Will save received');
+    });
 
-documents.listen(connection);
-connection.listen();
+    documents.listen(connection);
+    connection.listen();
